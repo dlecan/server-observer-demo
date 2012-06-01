3b7b7b23cde74291840acbb6a16ccc9d4a587b07
Damien Lecan <dev@dlecan.com>
2012-06-01 16:50:27 +0200

    Add Play2War

--- project/Build.scala
+++ project/Build.scala
@@ -4,15 +4,16 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "server-observer"
+    val appName         = "a_server-observer"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "com.github.play2war" %% "play2-war-core" % "0.4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here      
+      resolvers += "Play2war plugins release" at "http://repository-play-war.forge.cloudbees.com/release/"
     )
 
 }
